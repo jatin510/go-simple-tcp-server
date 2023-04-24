75ed9b1c5deac409d2a4d1a052f182287f299cf1
Jagdish Parihar <jatin6972@gmail.com>
2023-04-25 02:56:03 +0530

    basic tcp connection server

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":1729")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	do(conn)
+}
+
+func do(conn net.Conn) {
+	defer conn.Close()
+
+	// do something
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// processing
+	time.Sleep(1 * time.Second)
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello, World!\r\n"))
+
+}
